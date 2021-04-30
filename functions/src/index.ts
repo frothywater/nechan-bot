2b67369fd926c975ad70f5d36e718b58a593175f
frothywater <kokoroic@outlook.com>
2021-04-30 17:47:52 +0800

    Testing auto deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions"
 import bot from "./bot"
 
+// Testing auto deploy
+
 exports.bot = functions.https.onRequest(async (req, res) => {
     await bot.handleUpdate(req.body, res)
     res.end()
